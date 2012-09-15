b28a26d1aa8253509d3444cf8e5629e90b192a09
George Ornbo <george@shapeshed.com>
2012-09-15 21:18:24 +0100

    add unescapeHTML, removeAlpha, removeNonNumeric, removeNumeric

--- README.md
+++ README.md
@@ -16,12 +16,30 @@ Stringbean is a collection of string helper methods that don't clobber the `Stri
 
 ### escapeHTML
 
-Converts HTML tags to entities
+Escapes HTML to entities
 
     var someString = "<script>XSS Baby!</script>";
     console.log(stringbean.escapeHTML(someString));
 
-    &lt;script&gt;XSS Baby!&lt;/script&gt;
+    &lt;script&gt;XSS Baby!&lt;/script&gt
+
+### unescapeHTML
+
+Unescapes escaped HTML
+
+    var someString = "&lt;script&gt;alert(&quot;oh hai&quot;);&lt;/script&gt;";
+    console.log(stringbean.escapeHTML(someString));
+
+    <script>alert("oh hai")</script>;
+
+### removeAlpha
+
+Removes non alpha characters from a string
+
+    var someString = "ABC123def";
+    console.log(stringbean.removeAlpha(someString));
+
+    foo
 
 ### removeNonAlphanumeric
 
@@ -32,3 +50,20 @@ Removes non alphanumeric characters from a string
 
     foo
 
+### removeNonNumeric
+
+Removes non alphanumeric characters from a string
+
+    var someString = "ABCefg123hij";
+    console.log(stringbean.removeNonNumeric(someString));
+
+    123
+
+### removeNumeric
+
+Removes numeric characters from a string
+
+    var someString = "foo1a2b3cbar";
+    console.log(stringbean.removeNumeric(someString));
+
+    fooabcbar
--- test/stringbean.test.js
+++ test/stringbean.test.js
@@ -8,10 +8,38 @@ describe('when passing a string to the escapeHTML method', function(done){
     done();
   })
 })
+describe('when passing a string to the unescapeHTML method', function(done){
+  it('should correctly unescape escaped HTML', function(done){
+    var testString = "&lt;script&gt;alert(&quot;oh hai&quot;);&lt;/script&gt;";
+    assert.strictEqual("<script>alert(\"oh hai\");</script>", stringbean.unescapeHTML(testString));
+    done();
+  })
+})
+describe('when passing a string to the removeNumeric method', function(done){
+  it('should correctly remove numeric characters', function(done){
+    var testString = "ABC123def";
+    assert.strictEqual("ABCdef", stringbean.removeAlpha(testString));
+    done();
+  })
+})
 describe('when passing a string to the removeNonAlphanumeric method', function(done){
   it('should correctly remove non alphanumeric characters', function(done){
     var testString = "$£@@$%^foo(*&^%$£!@";
     assert.strictEqual("foo", stringbean.removeNonAlphanumeric(testString));
+    done();
+  })
+})
+describe('when passing a string to the removeNonNumeric method', function(done){
+  it('should correctly remove non numeric characters', function(done){
+    var testString = "ABCefg123hij";
+    assert.strictEqual("123", stringbean.removeNonNumeric(testString));
+    done();
+  })
+})
+describe('when passing a string to the removeNumeric method', function(done){
+  it('should correctly remove numeric characters', function(done){
+    var testString = "foo1a2b3cbar";
+    assert.strictEqual("fooabcbar", stringbean.removeNumeric(testString));
     done();
   })
 })
